056c90e30d92c33f18fa1cc0514158492f41844e
SaschaPWittwer <sascha.wittwer@bluewin.ch>
2017-11-20 18:49:03 +0100

    change naming for models

--- app/models/directory.js
+++ app/models/directory.js
@@ -1,7 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-    Id: DS.attr(),
-    Name: DS.attr('string'),
-    Path: DS.attr('string')
+    name: DS.attr('string'),
+    path: DS.attr('string')
 });
